fe31003dd36bffd8e9ed332a8b75273b48651d78
Michael Clerkin-Oliver <michael.clerkin-oliver@pushdr.com>
2020-10-03 17:44:44 +0100

    init

---
+++ Hello.World/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hello.World.Controllers
+{
+    [ApiController]
+    [Route("api/hello")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("hello");
+    }
+}
